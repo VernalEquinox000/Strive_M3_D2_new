0e380d6f9807f253a2a4eec234b3d9ae96fff4a1
VernalEquinox <pieroapruzzese@gmail.com>
2020-11-09 03:04:15 +0100

    working

--- index.js
+++ index.js
@@ -1,27 +1,70 @@
-async function loadSongs(artist) {
-    let response = await fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=" + artist, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-            "x-rapidapi-key": "575de39080mshf1f9cab8127c63fp1bcad8jsn113d9f3f814b"
-        }
-    })
-    let songs = await response.json();
-    return songs.data;
-}
+const loadAlbums = function (artist) {
+    fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q="+artist, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "8922e7eb38msha406b3662407a30p14661djsn13118198de68",
+		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+	}
+})
+        .then(response => response.json())
+        .then(apiResponse => {
 
+            let songs = apiResponse.data;
 
-let artists = ["clash", "kraftwerk", "mars volta", "smiths", "tool", "zu"]
+            let albumDiv = document.querySelector("#v-pills-home>div");
 
+            // ROW TITLE
+            let rowTitle = document.createTextNode("#TGIFRIDAY");
+            let rowTitleDiv = document.createElement("div");
+            rowTitleDiv.classList.add(
+                "row",
+                "row-cols-6",
+                "row-cols-md-6",
+                "row-cols-lg-6",
+                "row-cols-xl-6"
+            );
+            albumDiv.appendChild(rowTitleDiv);
+            let rowTitleCol = document.createElement("div");
+            rowTitleCol.classList.add("col");
+            rowTitleDiv.appendChild(rowTitleCol);
+            let rowTitleH1 = document.createElement("h1");
+            rowTitleH1.appendChild(rowTitle);
+            rowTitleCol.appendChild(rowTitleH1);
 
+            //ALBUM ROW
+            let albumRow = document.createElement("div");
+            albumRow.classList.add(
+                "row",
+                "row-cols-1",
+                "row-cols-sm-2",
+                "row-cols-md-3",
+                "row-cols-lg-4",
+                "row-cols-xl-6"
+            );
+            //albumRow.setAttribute(("style", "overflow-x: scroll; overflow-y: scroll; white-space: nowrap;"))
+            albumDiv.appendChild(albumRow);
 
-async function loadArtists() {
-    let songsList = document.querySelector("#songs")
-songsList.innerHTML=""
+              //albumsLocal = JSON.parse(localStorage.getItem("albums"));
+            for (let i = 0; i < 6; i++) {
+                let song = songs[i];
+    let albumCol = document.createElement("div");
+    albumCol.classList.add("col");
+    albumCol.innerHTML += `<div class="card mx-auto" style="width: 15 rem; display:inline block">
+                              <a href="album.html?${song.album.title}"
+                                ><img src="${song.album.cover_big}" class="card-img-top" alt="..."
+                              />
+                              <div class="play-btn rounded-pill"></div>
+                              </a>
+                            </div>
+                            <h6 class="card-title"><strong>${song.title}</strong></h6>
+                            <h6><strong>${song.artist.name}</strong></h6>
+                          </div>`;
+    albumRow.appendChild(albumCol);
+  }
+})
+	
+.catch(err => {
+	console.error(err);
+});
 
-    artists.forEach(async artist => {
-        let songs = await loadSongs(artist)
-        songsList.innerHTML += songs.map(song =>
-            `<p>$(song.title)</p>`).join("")
-    })
 }
